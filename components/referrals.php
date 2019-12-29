05c67146ad890bec4ab42ac2b4cd59f3fcea07e4
abraham28 <abrahampoog@gmail.com>
2019-12-29 16:53:56 +0800

    added css on referral list

--- components/referrals.php
+++ components/referrals.php
@@ -1,6 +1,80 @@
+<script>
+    document.addEventListener("DOMContentLoaded", function(event) {
+        $(".referralItem").click(function() {
+            console.log($(this).children(".subReferrals"));
+            $(this).children(".subReferrals").toggle();
+        })
+    });
+
+</script>
+
 <div class="referralBlk">
     <h2 class="referralBlkTitle">Your Referrals</h2>
     <ul class="directReferrals">
+        <li class="referralItem">
+            <div>Direct Referral</div>
+            <ul class="subReferrals">
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+            </ul>
+        </li>
+        <li class="referralItem">
+            <div>Direct Referral</div>
+            <ul class="subReferrals">
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+            </ul>
+        </li>
+        <li class="referralItem">
+            <div>Direct Referral</div>
+            <ul class="subReferrals">
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+            </ul>
+        </li>
+        <li class="referralItem">
+            <div>Direct Referral</div>
+            <ul class="subReferrals">
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+            </ul>
+        </li>
+        <li class="referralItem">
+            <div>Direct Referral</div>
+            <ul class="subReferrals">
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+            </ul>
+        </li>
+        <li class="referralItem">
+            <div>Direct Referral</div>
+            <ul class="subReferrals">
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+            </ul>
+        </li>
+        <li class="referralItem">
+            <div>Direct Referral</div>
+            <ul class="subReferrals">
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+            </ul>
+        </li>
+        <li class="referralItem">
+            <div>Direct Referral</div>
+            <ul class="subReferrals">
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+                <li class="subReferralItem">Sub Referral</li>
+            </ul>
+        </li>
         <li class="referralItem">
             <div>Direct Referral</div>
             <ul class="subReferrals">
